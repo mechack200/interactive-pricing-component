01b56bda6468394a62b4573f92ca6a25997793a9
mechack200 <taiwogboyegun@gmmail.com>
2021-06-15 14:13:46 +0100

    react pricing component created,and images file and curved, header, component added

---
+++ src/Components/header/Header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.component.styles.css';
+
+export const Header = () => {
+	return (
+		<div className="header_container">
+			{/* <div className="ring-logo">
+				<svg xmlns="http://www.w3.org/2000/svg" width="146" height="145">
+					<g fill="none" fill-rule="evenodd" stroke="#CFD8EF">
+						<circle cx="63" cy="82" r="62.5" />
+						<circle cx="105" cy="41" r="40.5" />
+					</g>
+				</svg>
+			</div>
+			<div className="introductory_text">
+				SImple, traffic-based pricing{' '}
+				<span> sign up for 30 days trial. No creadit card required</span>
+			</div> */}
+		</div>
+	);
+};
